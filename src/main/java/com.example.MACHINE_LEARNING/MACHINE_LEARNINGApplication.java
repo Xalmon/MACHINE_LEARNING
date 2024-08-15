9b13b4aeec192a636c9e684ede5da43f29379812
Xalmon <xalmonoyefule@gmail.com>
2024-08-15 14:16:18 +0100

    create a springboot project or bash, push to git and auto create branches and push to dockerhub

---
+++ src/main/java/com.example.MACHINE_LEARNING/MACHINE_LEARNINGApplication.java
@@ -0,0 +1,12 @@
+package com.example.MACHINE_LEARNING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MACHINE_LEARNINGApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MACHINE_LEARNINGApplication.class, args);
+  }
+}
